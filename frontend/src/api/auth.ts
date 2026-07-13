65ad248e0df9383b94d220e72423ec29d345a6fc
spotgalaxy <spotgalaxy_23@163.com>
2026-07-13 18:40:08 +0800

    去除前端文件

--- frontend/src/api/auth.ts
+++
@@ -1,6 +0,0 @@
-import request from '@/utils/request'
-import type { ApiResponse, LoginForm, LoginResult } from '@/types'
-
-export function login(data: LoginForm) {
-  return request.post<ApiResponse<LoginResult>>('/auth/login', data).then(res => res.data.data)
-}
