4ecc4a4a2ca5ebf0432d29e20cec6554a89d9800
spotgalaxy <spotgalaxy_23@163.com>
2026-07-13 15:13:29 +0800

    仪表盘的示例

---
+++ backend-module/src/main/java/com/campus/backend/service/DashboardService.java
@@ -0,0 +1,7 @@
+package com.campus.backend.service;
+
+import com.campus.common.vo.DashboardVO;
+
+public interface DashboardService {
+    DashboardVO getAdminDashboard();
+}
---
+++ common-module/src/main/java/com/campus/common/vo/DashboardVO.java
@@ -0,0 +1,12 @@
+package com.campus.common.vo;
+
+import lombok.Data;
+
+@Data
+public class DashboardVO {
+    private long classCount;
+    private long teacherCount;
+    private long studentCount;
+    private long courseCount;
+    private long pendingTaskCount;
+}
